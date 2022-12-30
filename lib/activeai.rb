50445dade0532c66345ed1bed2bbb02afbdad803
jeriko <dan@recombinary.com>
2022-12-30 21:25:22 +0200

    Gem base

---
+++ lib/activeai.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activeai/version"
+
+module Activeai
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/activeai_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activeai do
+  it "has a version number" do
+    expect(Activeai::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
